496a79f4281d9166d9f3940d69c0b9d0a2ede330
Aldrich Tabo <abtabo@up.edu.ph>
2025-11-09 18:09:55 +0800

    updated for unit testing to work as well
    Updated burn_trees and cut_tree functions to return the grid after modifications.

--- base_game/items_functionality.py
+++ base_game/items_functionality.py
@@ -1,14 +1,16 @@
 # Flame thrower functionality
 def burn_trees(grid, x, y):
     if not (0 <= x < len(grid) and 0 <= y < len(grid[0])):
-        return
+        return grid
     if grid[x][y] != "T":
-        return
+        return grid
     grid[x][y] = "." # burned trees turns to empty
     for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
         burn_trees(grid, x + dx, y + dy)
+    return grid
 
 
 # Axe functionality
 def cut_tree(grid, x, y):
     grid[x][y] = "."
+    return grid
