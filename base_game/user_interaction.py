2b1c3e99641d33199135f91602295d13a1e104e6
Kuretsuki <renzojosevega@gmail.com>
2025-11-10 16:15:31 +0800

    Refactor implement_game to support moves and output
    Refactor implement_game function to accept moves and output_file parameters. Update game logic to handle automated moves and file output for game state.

--- base_game/user_interaction.py
+++ base_game/user_interaction.py
@@ -4,16 +4,10 @@ from game_display import load_mapp, clear, TILE_EMOJIS
 from items_functionality import burn_trees, cut_tree
 from map_tracking import mapp, finding_L, finding_items, mushroom_counter
 
-def reset():
-    parser = ArgumentParser(description="Play Shroom Raider!")
-    parser.add_argument("stage_file", nargs = "?", default = "testmap.txt", help="Path to the stage file (e.g., testmap.txt)")
-    args = parser.parse_args()
-    implement_game(args.stage_file)
-
 collected_mush = [0]
 total_mush = [0]
 
-def implement_game(filename):
+def implement_game(filename, moves = None, output_file = None):
     directions = {
         "W":(-1, 0), 
         "A": (0, -1), 
@@ -24,6 +18,7 @@ def implement_game(filename):
     }
 
     clear()
+    status = "NO CLEAR"
     # Item-related variables
     pick_up_message = ""
     held_items = None
@@ -42,67 +37,78 @@ def implement_game(filename):
     # Display-related variables
     grid = [list(row) for row in mapp(filename)]
     first_display = ["".join(row) for row in grid]
-    load_mapp(first_display)
+
+    if moves is not None:
+        moves = list(moves.upper())
+    else:
+        clear()
+        load_mapp(first_display)
 
 
     while True:
-        print()
-        print(colored(f"{current_mush} ", "light_magenta") 
-            + "out of " 
-            + colored(f"{mushrooms} ", "light_magenta") 
-            + "mushroom(s) collected")
-        print()
-        print("[W] Move up")
-        print("[A] Move left")
-        print("[S] Move down")
-        print("[D] Move right")
-        print("[!] Reset")
-        print()
-        
-        if pick_up_message:  
-            print(pick_up_message)
-            pick_up_message = ""
-        else:
-            if current_loc in axes_loc:
-                if not held_items:
-                    print(colored(("There is an axe available for pickup!"), "yellow"))
-                else:
-                    print(colored("This item is unavailable for pickup.", "red"))
-            elif current_loc in fires_loc:
-                if not held_items:
-                    print(colored("There is a flamethrower available for pickup!", "yellow"))
-                else:
-                    print(colored("This item is currently unavailable for pickup.", "red"))
-            else:
-                print("There is no item here.")
-
-
-        if held_items == None:
-            print("Currently not holding anything.")
-        else:
-            print(f"Currently holding {TILE_EMOJIS[held_items]}")
-
-
-        # Getting input from user
-        movements = input("What is your next move?: ").upper()
-        if not movements:
-            clear()
-            partial_res = ["".join(row) for row in grid]
-            load_mapp(partial_res)
+        if moves is None:
             print()
-            print(colored("Invalid move! Use W/A/S/D/P/!.", "red"))
-            continue
+            print(colored(f"{current_mush} ", "magenta") 
+                + "out of " 
+                + colored(f"{mushrooms} ", "magenta") 
+                + "mushroom(s) collected")
+            print()
+            print("[W] Move up")
+            print("[A] Move left")
+            print("[S] Move down")
+            print("[D] Move right")
+            print("[!] Reset")
+            print()
             
+            if pick_up_message:  
+                print(pick_up_message)
+                pick_up_message = ""
+            else:
+                if current_loc in axes_loc:
+                    if not held_items:
+                        print(colored(("There is an axe available for pickup!"), "yellow"))
+                    else:
+                        print(colored("This item is unavailable for pickup.", "red"))
+                elif current_loc in fires_loc:
+                    if not held_items:
+                        print(colored("There is a flamethrower available for pickup!", "yellow"))
+                    else:
+                        print(colored("This item is currently unavailable for pickup.", "red"))
+                else:
+                    print("There is no item here.")
 
-        for movement in movements:
 
-            if movement not in directions:
+            if held_items == None:
+                print("Currently not holding anything.")
+            else:
+                print(f"Currently holding {TILE_EMOJIS[held_items]}")
+
+
+            # Getting input from user
+            movements = input("What is your next move?: ").upper()
+            if not movements:
                 clear()
                 partial_res = ["".join(row) for row in grid]
                 load_mapp(partial_res)
                 print()
                 print(colored("Invalid move! Use W/A/S/D/P/!.", "red"))
+                continue
+            movement_list = list(movements)
+        else:
+            if not moves:
                 break
+            movement_list = [moves.pop(0).upper()]
+                
+
+        for movement in movement_list:
+            if movement not in directions:
+                if moves is None:
+                    clear()
+                    partial_res = ["".join(row) for row in grid]
+                    load_mapp(partial_res)
+                    print()
+                    print(colored("Invalid move! Use W/A/S/D/P/!.", "red"))
+                continue
 
             x = current_loc[0]
             y = current_loc[1]
@@ -110,7 +116,7 @@ def implement_game(filename):
 
 
             if movement == "P":
-                clear()
+                clear() if moves is None else None
                 if previous_loc in ["*", "x"] and not held_items:  # if may item sa pinuntahang tile
                     axes_loc.remove(current_loc) if previous_loc == "x" else fires_loc.remove(current_loc)
                     held_items = previous_loc
@@ -118,22 +124,21 @@ def implement_game(filename):
                     previous_loc = "."
                 elif previous_loc not in ["*", "x"]:
                     pick_up_message = colored("There is no item to be picked up!", "red")
-
                 else:
                     pick_up_message = colored(f"You are already holding an item!", "red")
                         
                 grid[x][y] = "."  
                 grid[x + i][y + j] = "L"
                 current_loc = (x + i, y + j)
+                clear() if moves is None else None
                 partial_res = ["".join(row) for row in grid]
-
                 load_mapp(partial_res)
+                continue
 
             if movement == "!":
-                reset()
-                return     
+                return "RESET"
 
-            if 0 <= x + i < len(grid) and 0 <= y + j < len(grid[0]):
+            if 0 <= x + i < len(grid) and 0 <= y + j < len(grid[x + i]):
                 if grid[x + i][y + j] in [".", "_", "*", "x", "T"]: # if puwede daanan
                     if grid[x + i][y + j] == "T": # if tree
                         if held_items == "x":
@@ -145,19 +150,21 @@ def implement_game(filename):
                             held_items = None
                             pick_up_message = colored("You used a flamethrower!", "yellow")
                         else:
-                            clear()
+                            clear() if moves is None else None
                             partial_res = ["".join(row) for row in grid]
-                            load_mapp(partial_res)
-                            print(colored("\nThere's a tree blocking your path!", "red"))
+                            if moves is None:
+                                load_mapp(partial_res)
+                                print(colored("\nThere's a tree blocking your path!", "red"))
                             continue
 
                     grid[x][y] = previous_loc
                     previous_loc = grid[x + i][y + j]
                     grid[x + i][y + j] = "L"
                     current_loc = (x + i, y + j) 
-                    partial_res = ["".join(row) for row in grid]
-                    clear()
-                    load_mapp(partial_res)
+                    if moves is None:
+                        partial_res = ["".join(row) for row in grid]
+                        clear()
+                        load_mapp(partial_res)
                 
                 if grid[x + i][y + j] == "R": # if rock
                     if grid[x + 2*i][y + 2*j] ==".":
@@ -181,9 +188,10 @@ def implement_game(filename):
                         load_mapp(partial_res)
                         print(colored("You're not strong enough to push the rocks!", "red"))
                         continue
-                    partial_res = ["".join(row) for row in grid]
-                    clear()
-                    load_mapp(partial_res)
+                    if moves is None:
+                        partial_res = ["".join(row) for row in grid]
+                        clear()
+                        load_mapp(partial_res)
 
                 elif grid[x + i][y + j] == "+":
                     current_mush += 1
@@ -191,29 +199,51 @@ def implement_game(filename):
                     previous_loc = "."
                     grid[x + i][y + j] = "L"
                     current_loc = (x + i, y + j)
-                    partial_res = ["".join(row) for row in grid]
-                    clear()
-                    load_mapp(partial_res)
-
-                    if current_mush == mushrooms:
+                    if moves is None:
+                        partial_res = ["".join(row) for row in grid]
                         clear()
                         load_mapp(partial_res)
-                        print(colored("\nCongratulations!", "yellow"))
-                        print(f"\n{mushrooms} out of {mushrooms} mushroom(s) collected\n")
-                        return True 
+
+                    if current_mush == mushrooms:
+                        if moves is None:
+                            clear()
+                            load_mapp(partial_res)
+                            print(colored("\nCongratulations!", "yellow"))
+                            print(f"\n{mushrooms} out of {mushrooms} mushroom(s) collected\n")
+                            return True 
+                        else:
+                            status = "CLEAR"
+                        break
 
                 elif grid[x + i][y + j] == "~":
                     grid[x][y] = "."
                     current_loc = (x + i, y + j)
                     partial_res = ["".join(row) for row in grid]
                     collected_mush = current_mush
-                    clear()
+                    clear() if moves is None else None
                     load_mapp(partial_res)
-                    print(colored("\nGame Over!", "red"))
-                    print(colored("You fell in the water!"))
-                    print(f"\n{collected_mush} out of {mushrooms} mushroom(s) collected\n")
+                    if moves is None:
+                        print(colored("\nGame Over!", "red"))
+                        print(colored("You fell in the water!"))
+                        print(f"\n{collected_mush} out of {mushrooms} mushroom(s) collected\n")
+                        
+                    if output_file:
+                        with open(output_file, "w", encoding="utf-8") as f:
+                            f.write("NO CLEAR\n")
+                            for row in grid:
+                                f.write("".join(row) + "\n")
+
                     return False
             else:
                 partial_res = ["".join(row) for row in grid]
                 clear()
                 load_mapp(partial_res)
+
+        if moves is not None:
+            partial_res = ["".join(row) for row in grid]
+            if output_file:
+                with open(output_file, "w", encoding="utf-8") as f:
+                    f.write(status + "\n")
+                    for row in grid:
+                        f.write("".join(row) + "\n")
+    return status
