347b43de353e23e20c8605fa23028c259154266a
Kuretsuki <renzojosevega@gmail.com>
2025-11-10 16:15:07 +0800

    Refactor main function and enhance argument parsing
    Refactor main execution flow and add new command-line arguments for moves and output file.

--- base_game/main.py
+++ base_game/main.py
@@ -1,13 +1,39 @@
 from argparse import ArgumentParser
 from user_interaction import implement_game
 
-if __name__ == "__main__":
+def main():
     parser = ArgumentParser(description="Play Shroom Raider!")
     parser.add_argument(
-        "stage_file",
-        nargs="?",
+        "-f",
+        "--stage_file",
         default="testmap.txt",
         help="Path to the stage file (e.g., testmap.txt)",
     )
+    parser.add_argument(
+        "-m", 
+        "--moves", 
+        default=None, 
+        help="String of moves to simulate (e.g., WASDP!Q)")
+    parser.add_argument(
+        "-o", "--output", 
+        default=None, 
+        help="Output file for final map state")
     args = parser.parse_args()
-    implement_game(args.stage_file)
+    if args.moves:
+        result = implement_game(args.stage_file, moves = args.moves, output_file = args.output)
+        while result == "RESET":
+            result = implement_game(args.stage_file, moves=args.moves, output_file=args.output)
+        return
+
+    while True:
+        result = implement_game(args.stage_file)
+        if result == "RESET":
+            result = implement_game(args.stage_file)
+        else:
+            break
+        if result in ("CLEAR", "NO CLEAR"):
+            break
+
+
+if __name__ == "__main__":
+    main()
