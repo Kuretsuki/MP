7a9101e9d0e6e137e01d89a153632eaf3335b3ac
Aldrich Tabo <abtabo@up.edu.ph>
2025-11-13 16:34:21 +0800

    Refactor handle_movement to manage tile under rock
    Updated handle_movement function to include tile_under_rock parameter for better rock interaction handling.

--- base_game/game_main_controls.py
+++ base_game/game_main_controls.py
@@ -28,8 +28,7 @@ def handle_pickup(previous_loc, current_loc, axes_loc, fires_loc, held_items, gr
     grid[x][y] = "L"
     return previous_loc, held_items, pick_up_message
 
-
-def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mushrooms, multi_move = False, silent = False):
+def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mushrooms, tile_under_rock, multi_move = False, silent = False):
     current_loc = (x, y)
     next_x, next_y = x + i, y + j
     pick_up_message = ""
@@ -42,7 +41,7 @@ def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mu
             clear()
             load_mapp(partial_res)
             print(colored("\nYou cannot move outside the map!", "red"))
-        return (x, y), previous_loc, held_items, current_mush, None, pick_up_message
+        return (x, y), previous_loc, held_items, current_mush, None, pick_up_message, tile_under_rock
 
     target_cell = grid[next_x][next_y]
 
@@ -63,7 +62,7 @@ def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mu
                     clear()
                     load_mapp(partial_res)
                     print(colored("\nThere's a tree blocking your path!", "red"))
-                return (x, y), previous_loc, held_items, current_mush, None, pick_up_message
+                return (x, y), previous_loc, held_items, current_mush, None, pick_up_message, tile_under_rock
 
         grid[x][y] = previous_loc
         previous_loc = grid[next_x][next_y]
@@ -75,7 +74,7 @@ def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mu
         if delay and multi_move:
             time.sleep(0.25)
 
-        return (next_x, next_y), previous_loc, held_items, current_mush, None, pick_up_message
+        return (next_x, next_y), previous_loc, held_items, current_mush, None, pick_up_message, tile_under_rock
 
     # Pushing rocks
     elif target_cell == "R":
@@ -86,16 +85,16 @@ def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mu
                 clear()
                 load_mapp(partial_res)
                 print(colored("\nYou can't push the rock off the map!", "red"))
-            return (x, y), previous_loc, held_items, current_mush, None, pick_up_message
+            return (x, y), previous_loc, held_items, current_mush, None, pick_up_message, tile_under_rock
 
         next_cell = grid[next_two_x][next_two_y]
-
-        # If rock is pushed on an empty tile
+        
         if next_cell == ".":
             grid[next_two_x][next_two_y] = "R"
             grid[next_x][next_y] = "L"
             grid[x][y] = previous_loc
-            previous_loc = "."
+            previous_loc = tile_under_rock
+            tile_under_rock = "."
             current_loc = (next_x, next_y)
 
         # If rock is pushed in the water
@@ -106,11 +105,14 @@ def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mu
             previous_loc = "."
             current_loc = (next_x, next_y)
 
-        # If rock is pushed on a paved-tile
+
+        # If rock is pushed on a paved-tile 
         elif next_cell == "_":
             grid[next_two_x][next_two_y] = "R"
             grid[next_x][next_y] = "L"
-            grid[x][y] = "."
+            grid[x][y] = previous_loc
+            previous_loc = tile_under_rock
+            tile_under_rock = "_"  
             current_loc = (next_x, next_y)
 
         # If rock is pushed on a tree or mushroom
@@ -121,7 +123,7 @@ def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mu
                 clear()
                 load_mapp(partial_res)
                 print(colored(f"\nThere's a {obstacle} blocking your path!", "red"))
-            return (x, y), previous_loc, held_items, current_mush, None, pick_up_message
+            return (x, y), previous_loc, held_items, current_mush, None, pick_up_message, tile_under_rock
 
         # If two or more rock is pushed 
         elif next_cell == "R":
@@ -130,7 +132,7 @@ def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mu
                 clear()
                 load_mapp(partial_res)
                 print(colored("\nYou're not strong enough to push multiple rocks!", "red"))
-            return (x, y), previous_loc, held_items, current_mush, None, pick_up_message
+            return (x, y), previous_loc, held_items, current_mush, None, pick_up_message, tile_under_rock
 
         if silent is False:
             clear()
@@ -138,7 +140,7 @@ def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mu
         if delay and multi_move:
             time.sleep(0.15)
 
-        return current_loc, previous_loc, held_items, current_mush, None, pick_up_message
+        return current_loc, previous_loc, held_items, current_mush, None, pick_up_message, tile_under_rock
 
     # Collecting mushrooms
     elif target_cell == "+":
@@ -156,9 +158,9 @@ def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mu
             if silent is False:
                 print(colored("\nCongratulations!", "yellow"))
                 print(f"\n{mushrooms} out of {mushrooms} mushroom(s) collected\n")
-            return current_loc, previous_loc, held_items, current_mush, True, pick_up_message
+            return current_loc, previous_loc, held_items, current_mush, True, pick_up_message, tile_under_rock
 
-        return current_loc, previous_loc, held_items, current_mush, None, pick_up_message
+        return current_loc, previous_loc, held_items, current_mush, None, pick_up_message, tile_under_rock
 
     # Falling in the water - Losing State
     elif target_cell == "~":
@@ -170,4 +172,4 @@ def handle_movement(grid, x, y, i, j, held_items, previous_loc, current_mush, mu
             print(colored("\nGame Over!", "red"))
             print(colored("You fell in the water!"))
             print(f"\n{current_mush} out of {mushrooms} mushroom(s) collected\n")
-        return current_loc, previous_loc, held_items, current_mush, False, pick_up_message
+        return current_loc, previous_loc, held_items, current_mush, False, pick_up_message, tile_under_rock
